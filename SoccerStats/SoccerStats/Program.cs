a6423c3f9a0319d6c5468f2895342319408dce0b
Jizi Zhang <zjzottawa@gmail.com>
2017-03-10 01:39:44 -0500

    csv file added

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -13,17 +13,17 @@ namespace SoccerStats
 		{
 			string currentDirectory = Directory.GetCurrentDirectory();
 			DirectoryInfo directory = new DirectoryInfo(currentDirectory);
-			var fileName = Path.Combine(directory.FullName, "data.txt");
-			var file = new FileInfo(fileName);
-			if (file.Exists)
-			{
-				using (var reader = new StreamReader(file.FullName))
-				{
-					Console.SetIn(reader);
-					Console.WriteLine(Console.ReadLine());
-				}
+			var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+			var fileContents = ReadFile(fileName);
+			Console.WriteLine(fileContents);
+			
+		}
 
-				
+		public static string ReadFile(string fileName)
+		{
+			using (var reader = new StreamReader(fileName))
+			{
+				return reader.ReadToEnd();
 			}
 		}
 	}
